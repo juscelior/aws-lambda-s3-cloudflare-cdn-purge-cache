d5919cec5c8ed88d12905594d0d9113ea4092cae
juscelior <juscelio_reis@hotmail.com>
2017-06-12 11:58:52 -0300

    Clears the object cache on the CloudFlare CDN for each event in the S3 bucket

---
+++ Function.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.Lambda.S3Events;
+using System.Net.Http;
+using System.Text;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AWSLambdaS3.CloudFlarePurgeCache
+{
+    public class Function
+    {
+        IAmazonS3 S3Client { get; set; }
+
+        /// <summary>
+        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+        /// region the Lambda function is executed in.
+        /// </summary>
+        public Function()
+        {
+            S3Client = new AmazonS3Client();
+        }
+
+        /// <summary>
+        /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
+        /// </summary>
+        /// <param name="s3Client"></param>
+        public Function(IAmazonS3 s3Client)
+        {
+            this.S3Client = s3Client;
+        }
+
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+        /// to respond to S3 notifications.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
+        {
+            var s3Event = evnt.Records?[0].S3;
+            if (s3Event == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                HttpResponseMessage message;
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("X-Auth-Email", Environment.GetEnvironmentVariable("Email"));
+                    client.DefaultRequestHeaders.Add("X-Auth-Key", Environment.GetEnvironmentVariable("Key"));
+
+                    StringBuilder msg = new StringBuilder();
+                    msg.Append(@"{""files"":[""");
+                    msg.Append(Environment.GetEnvironmentVariable("Domain"));
+                    msg.Append("/");
+                    msg.Append(s3Event.Object.Key);
+                    msg.Append(@"""]}");
+
+                    StringContent content = new StringContent(msg.ToString(), Encoding.UTF8, "application/json");
+
+                    using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, new Uri(string.Format("https://api.cloudflare.com/client/v4/zones/{0}/purge_cache", Environment.GetEnvironmentVariable("Zone")))))
+                    {
+                        request.Content = content;
+                        message = await client.SendAsync(request);
+                    }
+                }
+
+                return await message.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+                context.Logger.LogLine(e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                throw;
+            }
+        }
+    }
+}
